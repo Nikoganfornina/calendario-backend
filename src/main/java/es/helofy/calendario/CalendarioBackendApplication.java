a3169c820f77c9041543ab1c47693d7206fefa63
Nicolas Ganfornina <niko@MacBook-Pro-de-Nicolas.local>
2026-02-07 08:55:52 +0100

    Initial spring boot project

---
+++ src/main/java/es/helofy/calendario/CalendarioBackendApplication.java
@@ -0,0 +1,13 @@
+package es.helofy.calendario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarioBackendApplication.class, args);
+	}
+
+}
